5708f1a4c5ff38a4a3ea9d7cf3d13725f05316e5
Yann Schüler <yschueler@stud.macromedia.de>
2023-10-27 15:57:43 +0200

    randomised placement and timing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from time import sleep
 import pyautogui as pg
 from pynput.keyboard import Key, Controller
+import random
 
-print(pg.size())
+counter = 0
+position = [-21, -18, -15, -12, -9, -6, -3, 0, 3, 6, 9, 12, 15, 18, 20]
 
 keyboard = Controller()
 
@@ -12,33 +14,36 @@ sleep(1)
 keyboard.release(Key.alt_l)
 keyboard.release(Key.alt_l)
 
-while True:
+while counter < 100:
 
     # open the game
-    pg.click(540, 940, duration=1)
+    pg.click(540+position[random.randint(0, 14)], 940+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
     sleep(10)
+    sleep(random.randint(0, 4))
 
     # collect elixir
-    pg.click(1650, 0, duration=1)
-    pg.click(1850, 1200, duration=1)
-    pg.click(2140, 150, duration=1)
+    pg.click(1650+position[random.randint(0, 14)], 21+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(1850+position[random.randint(0, 14)], 1200+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(2140+position[random.randint(0, 14)], 150+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
 
     # start battle
-    pg.click(150, 1300, duration=1)
-    pg.click(1900, 950, duration=1)
-    sleep(10)
+    pg.click(150+position[random.randint(0, 14)], 1300+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(1900+position[random.randint(0, 14)], 950+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    sleep(9)
+    sleep(random.randint(0, 4))
 
     # place troops
-    pg.click(600, 1300, duration=1)
-    pg.click(470, 360, duration=1)
-    pg.click(570, 200, duration=1)
-    pg.click(900, 60, duration=1)
-    pg.click(1600, 40, duration=1)
-    pg.click(1800, 220, duration=1)
-    pg.click(2050, 400, duration=1)
-    pg.click(440, 1300, duration=1)
-    pg.click(550, 1000, duration=1)
-    sleep(2)
+    pg.click(600+position[random.randint(0, 14)], 1300+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(470+position[random.randint(0, 14)], 360+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(570+position[random.randint(0, 14)], 200+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(900+position[random.randint(0, 14)], 60+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(1600+position[random.randint(0, 14)], 40+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(1800+position[random.randint(0, 14)], 220+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(2050+position[random.randint(0, 14)], 400+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(440+position[random.randint(0, 14)], 1300+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    pg.click(550+position[random.randint(0, 14)], 1000+position[random.randint(0, 14)], duration=1+random.randint(0, 2))
+    sleep(1)
+    sleep(random.randint(0, 4))
 
     # close the game
     keyboard.press(Key.alt_l)
@@ -46,3 +51,5 @@ while True:
     sleep(1)
     keyboard.release(Key.alt_l)
     keyboard.release(Key.f4)
+
+    counter += 1
